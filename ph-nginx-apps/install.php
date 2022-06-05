bebfddcbb9448f704d53f09988f164cd8fae4e8b
Edouard DAUBIN <edouard.daubin@gmail.com>
2022-06-05 13:54:52 +0200

    Next

---
+++ ph-nginx-apps/install.php
@@ -0,0 +1,231 @@
+<?php
+
+$envName = "test-nginx";
+$displayName = "displayName";
+
+/* require jelastic class file */
+require "./vendor/Jelastic.php";
+
+/* call Jelastic class */
+$jelastic = new Jelastic();
+
+echo "Login"."\n";
+
+$paramsSessionAdmin = array(
+	'appid' => $jelastic->JcaAppId,
+	'email' => $jelastic->apiUsername,
+	'password' => $jelastic->apiPassword
+);
+
+$sessionAdmin = $jelastic->login($paramsSessionAdmin);
+
+echo "Session = ".$sessionAdmin['session']."\n";
+
+if(isset($sessionAdmin['session']))
+{
+
+	echo "Instance creation "."\n";
+	
+	$env = $jelastic->getEnvInfo(
+		[
+			'envName' => $envName,
+			'session' => $sessionAdmin['session']
+		]
+	);
+	
+	print_r($env);
+	if($env["result"]==11) {
+			
+		$paramsRegAccount = array(
+			'appid' => $jelastic->JcaAppId,
+			'session' => $sessionAdmin['session'],
+			'jps' => 'https://raw.githubusercontent.com/Solutions-PH/jelastic-jps/main/nginx/manifest.jps',
+			'envName' => $envName,
+			'displayName' => $displayName,
+			'region' => 'thor'
+		);
+		
+		$createInstance = $jelastic->marketPlaceJpsInstall($paramsRegAccount);
+		
+		$f = fopen("./instance.json", "w");
+		fwrite($f, json_encode($createInstance));
+		fclose($f);
+		
+		$appid = $createInstance->appid;
+	
+	} else {
+		
+		$appid = "";
+		
+	}
+		
+	$createInstance = json_decode(file_get_contents("./instance.json"));
+	
+	echo "AppId = ".$appid."\n";
+	/*
+	$repos = $jelastic->getRepos([
+		'session' => $sessionAdmin['session'],
+		"appid" => "dashboard"
+	]);
+
+	$myRepos = [];
+	foreach($repos["array"] as $r) {
+		$myRepos[$r["name"]] = $r;
+	}
+
+	echo "Repository detected = ".count($myRepos)."\n";
+	*/
+	
+	$commands = [
+		[
+			"command" => "rm -f /etc/nginx/conf.d/nossl.conf && rm -f /etc/nginx/conf.d/ssl.conf && rm -f /etc/nginx/conf.d/virtual.conf && echo 'include /etc/nginx/conf.d/sites-enabled/*.*;' > /etc/nginx/conf.d/sites.conf",
+			"params" => ""
+		],[
+			"command" => "if grep -Fxq 'extension=intl.so' /etc/php.ini
+			then
+				echo 'ok';
+			else
+				echo 'extension=intl.so' >> /etc/php.ini;
+			fi",
+			"params" => ""
+		],[
+			"command" => "if grep -Fxq 'extension=gd.so' /etc/php.ini
+			then
+				echo 'ok';
+			else
+				echo 'extension=gd.so' >> /etc/php.ini;
+			fi",
+			"params" => ""
+		],[
+			"command" => "if grep -Fxq 'extension=pdo.so' /etc/php.ini
+			then
+				echo 'ok';
+			else
+				echo 'extension=pdo.so' >> /etc/php.ini;
+			fi",
+			"params" => ""
+		],[
+			"command" => "if grep -Fxq 'extension=pdo_dblib.so' /etc/php.ini
+			then
+				echo 'ok';
+			else
+				echo 'extension=pdo_dblib.so' >> /etc/php.ini;
+			fi",
+			"params" => ""
+		],[
+			"command" => "if grep -Fxq 'extension=imagick.so' /etc/php.ini
+			then
+				echo 'ok';
+			else
+				echo 'extension=imagick.so' >> /etc/php.ini;
+			fi",
+			"params" => ""
+		]
+	];
+		
+	$cmd = $jelastic->execCmd([
+		"envName" => $envName,
+		"session" => $sessionAdmin['session'],
+		"nodeGroup" => "cp",
+		"commandList" => json_encode($commands),
+	]);
+	
+	$sites = yaml_parse(file_get_contents('./sites.yaml'));
+
+	foreach($sites as $site) {
+
+		echo "Start : ".$site["name"]."\n";
+		/*
+		if(array_key_exists("repo", $site)) {
+			if(!array_key_exists($site["repo"]["name"], $myRepos)) {
+				echo "Repository creation : ".$site["repo"]["name"]."\n";
+				
+				$repos = $jelastic->addRepo([
+					"appid" => "dashboard",
+					"session" => $sessionAdmin['session'],
+					"name" => $site["repo"]["name"],
+					"url" => $site["repo"]["url"],
+					"keyId" => 506
+				]);
+								
+			}
+		}
+		*/
+		
+		echo "Deploy: ".$site["name"]."\n";
+		
+		$repos = $jelastic->deploy([
+			"envName" => $envName,
+			"session" => $sessionAdmin['session'],
+			"repo" => '{"url":"'.$site["repo"]["url"].'", "branch":"main","keyId":506}',
+			"context" => $site["repo"]["name"],
+			"nodeGroup" => "cp",
+			"settings" => '{"autoResolveConflict": "true", "autoUpdate": "true", "autoUpdateInterval": "1"}'
+		]);
+		
+		print_R($repos);
+		
+		/*
+		$commads = [
+			[
+				"command" => "cd /var/www/webroot/ && wget https://raw.githubusercontent.com/Solutions-PH/jelastic-jps/main/nginx/scripts/install.php && php install.php",
+				"params" => ""
+			]
+		];
+		
+		$repos = $jelastic->execCmd([
+			"envName" => $envName,
+			"session" => $sessionAdmin['session'],
+			"nodeGroup" => "cp",
+			"commandList" => json_encode($commands),
+		]);
+		*/
+		echo "End : ".$site["name"]."\n";
+		echo "---------\n";
+
+		exit;
+	}
+
+	exit;
+	
+	print_r($sites);
+	
+	if(isset($createAccount['object']['activationKey']))
+	{
+		$paramsEnableAccount = array(
+			'appid' => $jelastic->signUpAppId,
+			'session' => $sessionAdmin['session'],
+			'script' => 'activate',
+			'activationKey' => $createAccount['object']['activationKey'],
+			'password' => $password,
+			'group' => $jelastic->group,
+			'key' => $createAccount['object']['activationKey']
+		);
+		$enableAccount = $jelastic->enableAccount($paramsEnableAccount);
+		if($enableAccount) {
+			$paramsGetUid = array(
+				'appid' => $jelastic->JcaAppId,
+				'session' => $sessionAdmin['session'],
+				'login' => $email
+			);
+			$getUid = $jelastic->getUid($paramsGetUid);
+			if(isset($getUid['uid'])) {
+				echo "Create account success.<br>Email : ".$email."<br>Password : ".$password."<br>Uid : ".$getUid['uid'];
+			} else {
+				echo "Get UID account failed.";
+			}
+		} else {
+			echo "Enable account failed.";
+		}
+	}
+	else
+	{
+		echo "Create account failed.";
+	}
+}
+else
+{
+	echo "Login as admin failed.";
+}
+
+?>
---
+++ ph-nginx-apps/scripts/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+yum -y install https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm
+
+yum-config-manager --enable remi
+
+yum -y install ImageMagick7-heic
+
+php install.php
