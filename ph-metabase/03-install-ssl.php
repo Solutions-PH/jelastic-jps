e0779a02c4b48641b7aeb72b2fb1e0de35e8cb9f
Edouard DAUBIN <edouard.daubin@gmail.com>
2022-07-05 23:48:14 +0200

    ssl

--- ph-metabase/03-install-ssl.php
+++ ph-metabase/03-install-ssl.php
@@ -38,55 +38,27 @@ if(isset($sessionAdmin['session']))
 					
 		echo "AppId = ".$appid."\n";
 	
-		$sites = yaml_parse(file_get_contents('./config.yaml'));
+		echo "Nginx configuration"."\n";
 		
-		echo "Domain configuration"."\n";
-		
-		foreach($sites["certificates"] as $site) {
-		
-			echo "Start : ".$site["domain"]."\n";		
+		$command = "cd /etc/nginx/sites-enabled && php -r \"copy('https://raw.githubusercontent.com/Solutions-PH/jelastic-jps/main/ph-metabase/nginx/default.ssl.conf', 'default.ssl.conf');\"";
+				
+		$commands = [
+			[
+				"command" => $command,
+				"params" => ""
+			]
+		];
 			
-			echo "Nginx configuration"."\n";
-			
-			$command = "cd /etc/nginx/conf.d/sites-enabled-ssl && php -r \"copy('https://raw.githubusercontent.com/Solutions-PH/jelastic-jps/main/ph-nginx-apps/nginx/template.ssl.conf', '".$site["domain"].".conf');\"";
-					
-			$commands = [
-				[
-					"command" => $command,
-					"params" => ""
-				],[
-					"command" => "sed -i 's/#server_name#/".$site["domain"]."/g' /etc/nginx/conf.d/sites-enabled-ssl/".$site["domain"].".conf",
-					"params" => ""
-				],[
-					"command" => "sed -i 's/#server_path#/".$site["distinguished_name"]["organization_unit_name"]."/g' /etc/nginx/conf.d/sites-enabled-ssl/".$site["domain"].".conf",
-					"params" => ""
-				]
-			];
-			
-			if(array_key_exists("subject_alternative_names", $site)) {
-				$commands[] = [
-					"command" => "sed -i 's/#server_alt_name#/".$site["subject_alternative_names"]."/g' /etc/nginx/conf.d/sites-enabled-ssl/".$site["domain"].".conf",
-					"params" => ""
-				];
-			} else {
-				$commands[] = [
-					"command" => "sed -i 's/#server_alt_name#/ /g' /etc/nginx/conf.d/sites-enabled-ssl/".$site["domain"].".conf",
-					"params" => ""
-				];
-			}
-			
-			$cmd = $jelastic->execCmd([
-				"envName" => $envName,
-				"session" => $sessionAdmin['session'],
-				"nodeGroup" => "cp",
-				"commandList" => json_encode($commands),
-			]);
-	
-			echo "End : ".$site["domain"]."\n";
-			echo "---------\n";
-			
-		}
-		
+		$cmd = $jelastic->execCmd([
+			"envName" => $envName,
+			"session" => $sessionAdmin['session'],
+			"nodeGroup" => "vps",
+			"commandList" => json_encode($commands),
+		]);
+
+		echo "End : bi.7.fr\n";
+		echo "---------\n";
+				
 
 	}
 
