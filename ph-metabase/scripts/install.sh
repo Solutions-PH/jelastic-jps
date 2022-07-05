229d2ff347aa799cd2cb044d5d9a1c579deeeb5f
Edouard DAUBIN <edouard.daubin@gmail.com>
2022-07-05 19:36:00 +0200

    Update install.sh

--- ph-metabase/scripts/install.sh
+++ ph-metabase/scripts/install.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-MB_PATH_DEFAULT="/opt/metabase"
-MB_VERSION_DEFAULT="v0.43.4"
+MB_PATH="/opt/metabase"
+MB_VERSION="v0.43.4"
 
 echo "Hello, "$USER".  This script will Configure. Install & run metabase on port localhost:3000"
 echo -n "Enter your directory to install metabase e.g,.. '/opt/metabase' and press [ENTER] : "
 
-MB_PATH="/opt/metabase"
 echo "selected directory will be $MB_PATH"
 
 echo -n "Enter your version of metabase that is needed See https://github.com/metabase/metabase/releases  e.g,.. 'v0.35.0' and press [ENTER]: "
-read $MB_VERSION\n
-MB_VERSION="${MB_VERSION:-$MB_VERSION_DEFAULT}"
+
 echo "selected version will be $MB_VERSION"
 
 apt-get update -qy
