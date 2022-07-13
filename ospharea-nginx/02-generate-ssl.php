00c470f9bec40f95780caaf88ae95ee41377e61e
Edouard DAUBIN <edouard.daubin@gmail.com>
2022-07-13 15:40:23 +0200

    Push

---
+++ ospharea-nginx/02-generate-ssl.php
@@ -0,0 +1,63 @@
+<?php
+
+/* require config file */
+require "./00-config.php";
+
+/* require jelastic class file */
+require "./vendor/Jelastic.php";
+
+/* call Jelastic class */
+$jelastic = new Jelastic();
+
+echo "Login"."\n";
+
+$paramsSessionAdmin = array(
+	'appid' => $jelastic->JcaAppId,
+	'email' => $jelastic->apiUsername,
+	'password' => $jelastic->apiPassword
+);
+
+$sessionAdmin = $jelastic->login($paramsSessionAdmin);
+
+echo "Session = ".$sessionAdmin['session']."\n";
+
+if(isset($sessionAdmin['session']))
+{
+
+	echo "Instance check "."\n";
+
+	$env = $jelastic->getEnvInfo(
+		[
+			'envName' => $envName,
+			'session' => $sessionAdmin['session']
+		]
+	);
+		
+	if($env["result"]==0) {
+		$appid = $env["env"]["appid"];		
+					
+		$command = "cd /var/www/webroot && php -r \"copy('https://raw.githubusercontent.com/Solutions-PH/jelastic-jps/main/".$envName."/config.yaml', 'config.yaml');\" && php acmephp.phar run config.yaml";
+				
+		$commands = [
+			[
+				"command" => "cd /var/www/webroot && php -r \"copy('https://github.com/acmephp/acmephp/releases/download/2.0.0/acmephp.phar', 'acmephp.phar');\" && php -r \"copy('https://github.com/acmephp/acmephp/releases/download/2.0.0/acmephp.phar.pubkey', 'acmephp.phar.pubkey');\"",
+				"params" => ""
+			],
+			[
+				"command" => $command,
+				"params" => ""
+			]
+		];
+		
+		$cmd = $jelastic->execCmd([
+			"envName" => $envName,
+			"session" => $sessionAdmin['session'],
+			"nodeGroup" => "cp",
+			"commandList" => json_encode($commands),
+		]);
+		
+		print_r($cmd);
+
+	}
+}
+?>
---
+++ ospharea-nginx/03-install-ssl.php
@@ -0,0 +1,113 @@
+<?php
+
+/* require config file */
+require "./00-config.php";
+
+/* require jelastic class file */
+require "./vendor/Jelastic.php";
+
+/* call Jelastic class */
+$jelastic = new Jelastic();
+
+echo "Login"."\n";
+
+$paramsSessionAdmin = array(
+	'appid' => $jelastic->JcaAppId,
+	'email' => $jelastic->apiUsername,
+	'password' => $jelastic->apiPassword
+);
+
+$sessionAdmin = $jelastic->login($paramsSessionAdmin);
+
+echo "Session = ".$sessionAdmin['session']."\n";
+
+if(isset($sessionAdmin['session']))
+{
+
+	echo "Instance check "."\n";
+
+	$env = $jelastic->getEnvInfo(
+		[
+			'envName' => $envName,
+			'session' => $sessionAdmin['session']
+		]
+	);
+		
+	if($env["result"]==0) {
+		$appid = $env["env"]["appid"];		
+					
+		echo "AppId = ".$appid."\n";
+	
+		$sites = yaml_parse(file_get_contents('./config.yaml'));
+		
+		echo "Domain configuration"."\n";
+		
+		foreach($sites["certificates"] as $site) {
+		
+			echo "Start : ".$site["domain"]."\n";		
+			
+			echo "Nginx configuration"."\n";
+			
+			$command = "cd /etc/nginx/conf.d/sites-enabled-ssl && php -r \"copy('https://raw.githubusercontent.com/Solutions-PH/jelastic-jps/main/".$envName."/nginx/template.ssl.conf', '".$site["domain"].".conf');\"";
+			
+			$path = str_replace("/var/www/webroot/", "", $site["solver"]["root"]);
+			$path = str_replace("/public", "", $path);
+			
+			$publicPath = str_replace("/", "\/", $path);
+
+			$commands = [
+				[
+					"command" => $command,
+					"params" => ""
+				],[
+					"command" => "sed -i 's/#server_name#/".$site["domain"]."/g' /etc/nginx/conf.d/sites-enabled-ssl/".$site["domain"].".conf",
+					"params" => ""
+				],[
+					"command" => "sed -i 's/#server_path#/".$publicPath."/g' /etc/nginx/conf.d/sites-enabled-ssl/".$site["domain"].".conf",
+					"params" => ""
+				]
+			];
+			
+			if(array_key_exists("subject_alternative_names", $site)) {
+				$commands[] = [
+					"command" => "sed -i 's/#server_alt_name#/".$site["subject_alternative_names"]."/g' /etc/nginx/conf.d/sites-enabled-ssl/".$site["domain"].".conf",
+					"params" => ""
+				];
+			} else {
+				$commands[] = [
+					"command" => "sed -i 's/#server_alt_name#/ /g' /etc/nginx/conf.d/sites-enabled-ssl/".$site["domain"].".conf",
+					"params" => ""
+				];
+			}
+			
+			$cmd = $jelastic->execCmd([
+				"envName" => $envName,
+				"session" => $sessionAdmin['session'],
+				"nodeGroup" => "cp",
+				"commandList" => json_encode($commands),
+			]);
+			
+			print_r($cmd);
+	
+			echo "End : ".$site["domain"]."\n";
+			echo "---------\n";
+			
+		}
+		
+
+	}
+
+	echo "Restart : ".$envName."\n";
+	
+	foreach($env["nodes"] as $node) {
+		
+		$cmd = $jelastic->restart([
+			"envName" => $envName,
+			"nodeId" => $node["id"],
+			"session" => $sessionAdmin['session']
+		]);
+					
+	}
+	
+}
+?>
